a0fbdd069368526da21b22f8a6e4d50b3a0ba969
Brian Klug <brian.klug@gmail.com>
2010-08-27 11:58:20 -0700

    Added log, bugfixes

--- main.py
+++ main.py
@@ -16,6 +16,13 @@ class Sign(db.Model):
   who          = db.UserProperty()
   when         = db.DateTimeProperty(auto_now=True)
 
+class Log(db.Model):
+  two_digit_id = db.StringProperty()
+  name         = db.StringProperty()
+  message      = db.StringProperty(multiline=True)
+  who          = db.UserProperty()
+  when         = db.DateTimeProperty(auto_now=True)
+
 class MainHandler(webapp.RequestHandler):
   def get(self):
     user = users.get_current_user()
@@ -38,6 +45,8 @@ class MainHandler(webapp.RequestHandler):
           s.who = user
           s.changed = True
           s.put()
+          log = Log(message=newmessage,who=user,name=s.name,two_digit_id=s.two_digit_id)
+          log.put()
         self.redirect('/')
 
 class ApiHandler(webapp.RequestHandler):
@@ -54,21 +63,30 @@ class ApiAllHandler(webapp.RequestHandler):
 class ResetHandler(webapp.RequestHandler):
   def get(self):
     for m in Sign.all().fetch(100):
+      m.message = m.default_msg
+      m.changed = True
+      m.who = null
+      m.put()
+    self.response.out.write("OK")
+
+class CreateHandler(webapp.RequestHandler):
+  def get(self):
+    for m in Sign.all().fetch(100):
       m.delete()
-    s = Sign(changed=True,name="Lobby Left",two_digit_id="04",default_msg="Welcome to\nHacker Dojo")
+    s = Sign(changed=True,name="Lobby Left",two_digit_id="04",default_msg="Welcome to\r\nHacker Dojo")
     s.put()
     s = Sign(changed=True,name="Lobby Center",two_digit_id="30",default_msg="Welcome")
     s.put()
-    s = Sign(changed=True,name="Lobby Right",two_digit_id="05",default_msg="Please sign in\nEnjoy your visit")
+    s = Sign(changed=True,name="Lobby Right",two_digit_id="05",default_msg="Please sign in\r\nEnjoy your visit")
     s.put()
     s = Sign(changed=True,name="140B",two_digit_id="40",default_msg="140B")
     s.put()
     s = Sign(changed=True,name="Savanna",two_digit_id="03",default_msg="Savanna")
     s.put()
-    self.redirect('/')
+    self.response.out.write("OK")
     
 def main():
-   application = webapp.WSGIApplication([('/', MainHandler),('/api', ApiHandler),('/api/all', ApiAllHandler),('/reset', ResetHandler)], debug=True)
+   application = webapp.WSGIApplication([('/', MainHandler),('/api', ApiHandler),('/api/all', ApiAllHandler),('/reset', ResetHandler),('/create', CreateHandler)], debug=True)
    util.run_wsgi_app(application)
 
 if __name__ == '__main__':
